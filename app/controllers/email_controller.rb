5746faf7f4d6b09d813579c09c90667776f9f779
William <richerd.jesus@gmail.com>
2010-07-06 23:40:43 +0530

    email

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -14,8 +14,7 @@ protect_from_forgery :only => [:update, :delete, :create]
   
   # Include Helper class
   def message
-  puts "!!!!!!!!!!!!!!!!!!!!!!"
-  puts params[:email]
+render :text => params[:email]
 	mail = TMail::Mail.parse(params[:email])	
   description = mail.subject  
 	mailstr = mail.to
