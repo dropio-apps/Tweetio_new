f1e9fdcf67be186938fb110dafa1925da9e28102
William <richerd.jesus@gmail.com>
2010-07-07 21:55:20 +0530

    email route added

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -14,7 +14,7 @@ protect_from_forgery :only => [:update, :delete, :create]
   
   # Include Helper class
   def message
-    emails = "/tmp/" +  params[:email].to_s
+    emails = "/tmp/" +  params[:id].to_s
     file = File.open(emails)
     contents = file.read
     file.close
